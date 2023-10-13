ab7b220992e38efec6791f06d329e6e588fa5ba1
Henrik Steen <henrist@hiof.no>
2023-10-13 16:37:24 +0200

    added navigation-compose dependency

--- app/src/main/java/com/example/joggis/RegistrationActivity.kt
+++ app/src/main/java/com/example/joggis/RegistrationActivity.kt
@@ -1,45 +1,4 @@
 package com.example.joggis
 
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.FirebaseUser
-
-class RegistrationActivity : AppCompatActivity() {
-    private lateinit var mAuth: FirebaseAuth
-    private lateinit var emailEditText: EditText
-    private lateinit var passwordEditText: EditText
-    private lateinit var registerButton: Button
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
-
-        mAuth = FirebaseAuth.getInstance()
-        emailEditText = findViewById(R.id.emailEditText)
-        passwordEditText = findViewById(R.id.passwordEditText)
-        registerButton = findViewById(R.id.registerButton)
-
-        registerButton.setOnClickListener {
-            val email = emailEditText.text.toString()
-            val password = passwordEditText.text.toString()
-
-            mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, OnCompleteListener<AuthResult> { task ->
-                    if (task.isSuccessful) {
-                        // Registrering vellykket
-                        val user: FirebaseUser? = mAuth.currentUser
-                        // Utfør nødvendige handlinger etter registrering
-                    } else {
-                        // Registrering mislyktes, gi brukeren beskjed
-                    }
-                })
-        }
-    }
+class RegistrationActivity {
 }
