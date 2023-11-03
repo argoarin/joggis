6510cf4c2c41bb34545a56b590a7a675884a5b54
Henrik Steen <henrist@hiof.no>
2023-11-03 10:02:39 +0100

    improved login/register, confirmed its adding users to firebase, added a third page HomePage with placeholders

--- app/src/main/java/com/example/joggis/UI.kt
+++ app/src/main/java/com/example/joggis/UI.kt
@@ -1,5 +1,6 @@
 package com.example.joggis
 
+import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,8 +17,10 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.NavController
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 /*
 UI currently has Home Screen with Login/Register button
@@ -26,7 +29,18 @@ Login/Register navigates to Login/Register page
 object UI {
 
     @Composable
-    fun HomeScreen(navController: NavController) {
+    fun AppNavigator() {
+        val navController = rememberNavController()
+
+        NavHost(navController = navController, startDestination = "startup") {
+            composable("startup") { StartupScreen(navController) }
+            composable("loginRegister") { LoginRegisterScreen(navController) }
+            composable("home") { HomePage(navController) }
+            // Other destinations...
+        }
+    }
+    @Composable
+    fun StartupScreen(navController: NavController) {
         Box(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
@@ -48,92 +62,149 @@ object UI {
     fun LoginRegisterScreen(navController: NavController) {
         var email by remember { mutableStateOf("") }
         var password by remember { mutableStateOf("") }
-        var loginError by remember { mutableStateOf<String?>(null) }
+        var message by remember { mutableStateOf("") }
+        var isLoading by remember { mutableStateOf(false) }
 
-        val firebaseAuth = FirebaseAuth.getInstance()
+        // Function to handle the login button click
+        fun handleLogin() {
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                isLoading = true
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        isLoading = false
+                        if (task.isSuccessful) {
+                            message = "Successfully logged in as $email!"
+                            navController.navigate("home") {
+                                popUpTo("loginRegister") { inclusive = true }
+                            }
+                        } else {
+                            message = task.exception?.localizedMessage ?: "Login failed"
+                        }
+                    }
+            } else {
+                message = "Email and password cannot be empty."
+            }
+        }
+
+        // Function to handle the register button click
+        fun handleRegister() {
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                isLoading = true
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        isLoading = false
+                        if (task.isSuccessful) {
+                            message = "Registered $email! You can now log in."
+                        } else {
+                            message = task.exception?.localizedMessage ?: "Registration failed"
+                        }
+                    }
+            } else {
+                message = "Email and password cannot be empty."
+            }
+        }
 
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier.padding(16.dp)
-            ) {
-                TextField(
-                    value = email,
-                    onValueChange = { email = it },
-                    label = { Text("E-mail Address") },
-                    placeholder = { Text("Enter e-mail") }
-                )
-
-                Spacer(modifier = Modifier.height(8.dp))
-
-                TextField(
-                    value = password,
-                    onValueChange = { password = it },
-                    label = { Text("Password") },
-                    placeholder = { Text("Enter password") },
-                    visualTransformation = PasswordVisualTransformation()
-                )
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Button(onClick = {
-                    firebaseAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener { task ->
-                            if (task.isSuccessful) {
-                                // Sign in success, navigate to home or other destination
-                                navController.popBackStack()
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                loginError = task.exception?.message
-                            }
-                        }
-                }) {
-                    Text("Register")
-                }
-
-                Spacer(modifier = Modifier.height(8.dp))
-
-                Button(onClick = {
-                    firebaseAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener { task ->
-                            if (task.isSuccessful) {
-                                // Sign in success, navigate to home or other destination
-                                navController.popBackStack()
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                loginError = task.exception?.message
-                            }
-                        }
-                }) {
-                    Text("Login")
-                }
-
-                loginError?.let {
-                    Text(text = it, color = MaterialTheme.colors.error)
+            if (isLoading) {
+                CircularProgressIndicator()
+            } else {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    if (message.isNotEmpty()) {
+                        Text(text = message, color = MaterialTheme.colors.error)
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+                    TextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text("Email") },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth(0.8f)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    TextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Password") },
+                        visualTransformation = PasswordVisualTransformation(),
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth(0.8f)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = { handleLogin() }, modifier = Modifier.fillMaxWidth(0.8f)) {
+                        Text("Login")
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Button(onClick = { handleRegister() }, modifier = Modifier.fillMaxWidth(0.8f)) {
+                        Text("Register")
+                    }
                 }
             }
         }
     }
-
-
-    @Composable
-    fun AppNavigator() {
-        val navController = rememberNavController()
-
-        NavHost(navController = navController, startDestination = "home") {
-            composable("home") {
-                HomeScreen(navController)
-            }
-            composable("loginRegister") {
-                LoginRegisterScreen(navController)
-            }
-        }
-    }
-
-
 
 }
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomePage(navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Joggis") }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding), // Apply padding to avoid overlapping with the TopAppBar
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Register Activity Box
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .clickable { /* Handle click, navigate to register activity */ },
+                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+            ) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Text(text = "Register Activity", style = MaterialTheme.typography.h6)
+                }
+            }
+
+            // Activities Box
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .clickable { /* Handle click, navigate to activities list */ },
+                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+            ) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Text(text = "Activities", style = MaterialTheme.typography.h6)
+                }
+            }
+        }
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
