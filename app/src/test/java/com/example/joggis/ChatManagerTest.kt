190f136c8031fa67e6120747fba020c53923acca
Henrik Steen <henrist@hiof.no>
2023-11-27 04:14:10 +0100

    renamed test

--- app/src/test/java/com/example/joggis/ChatManagerTest.kt
+++ app/src/test/java/com/example/joggis/ChatManagerTest.kt
@@ -6,7 +6,6 @@ import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import org.junit.Test
 
-import org.junit.Assert.*
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
@@ -20,24 +19,22 @@ class ChatManagerTest {
 
     @Test
     fun testSendMessage() {
-        // Arrange
-        val chat = Chat(/* parameters */)
+        val chat = Chat()
         val mockCollection = mock(CollectionReference::class.java)
-        val mockTask = mock(Task::class.java) as Task<DocumentReference> // Mock the Task object
+        val mockTask = mock(Task::class.java) as Task<DocumentReference>
 
         `when`(mockDb.collection("chat")).thenReturn(mockCollection)
         `when`(mockCollection.add(chat)).thenReturn(mockTask)
 
-        // Optionally, mock the behavior of addOnSuccessListener and addOnFailureListener
         `when`(mockTask.addOnSuccessListener(any())).thenReturn(mockTask)
         `when`(mockTask.addOnFailureListener(any())).thenReturn(mockTask)
 
-        // Act
-        chatManager.sendMessage(chat, {}, { /* Handle failure */ })
 
-        // Assert
+        chatManager.sendMessage(chat, {}, {})
+
+
         verify(mockCollection).add(chat)
-        // Optionally, verify that addOnSuccessListener and addOnFailureListener are called
+
         verify(mockTask).addOnSuccessListener(any())
         verify(mockTask).addOnFailureListener(any())
     }
